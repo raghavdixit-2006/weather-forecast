0bb7c62f3e8674bb6e45a462a6b1fb4481421304
RishabhOnGit <rishabhrajput.gitpersonal@gmail.com>
2025-04-23 14:35:43 +0530

    fixed the issue raised by the owner

--- script2.js
+++ script2.js
@@ -18,3 +18,22 @@ let name = document.querySelector("#name");
 let state = document.querySelector("#state");
 let country = document.querySelector("#country");
 let lon = document.querySelector("#lon");
+let lat = document.querySelector("#lat");
+async function getIPInfo(){
+    const response = await fetch(ipinformation);
+    var info = await response.json();
+    console.log(info);
+
+    const response2 = await fetch(apikey + info.city);
+	var data = await response2.json();
+    console.log(data);
+
+    feels.innerHTML = `${Math.round(data.main.feels_like)}°C`;
+    maxtemp.innerHTML = `${Math.round(data.main.temp_max)}°C`;
+    mintemp.innerHTML = `${Math.round(data.main.temp_min)}°C`;
+    humidity.innerHTML = `${data.main.humidity}%`;
+    pressure.innerHTML = `${data.main.pressure} hPa`;
+    sealev.innerHTML = `${data.main.sea_level} hPa`;
+    speed.innerHTML = `${data.wind.speed} m/s`;
+    gndlev.innerHTML = `${data.main.grnd_level} hPa`;
+    Visibility.innerHTML = `${data.visibility/1000} Km`;
